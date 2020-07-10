3071711c02195fe98d468814f35b3650f2f6ae93
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-07-11 01:08:54 +0300

    made slider

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@ const btnModalClose = document.querySelector('.connection__close');
 const login = document.querySelector('.login');
 const password = document.querySelector('.password');
 const message = document.querySelector('.message');
+const wrapper = document.querySelector('.site-wrapper');
+const sliderElement = document.querySelectorAll('.slider-item');
+const btnSlider = document.querySelectorAll('.slider-btn');
+
+function clean(wrapper, slider, buttons){
+  slider.forEach(element=>{
+    element.classList.remove('active')
+  });
+  buttons.forEach(element=>{
+    element.classList.remove('active')
+  });
+  wrapper.classList.remove('wrapper-1');
+  wrapper.classList.remove('wrapper-2');
+  wrapper.classList.remove('wrapper-3');
+}
 
 btnModalOpen.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -14,23 +29,43 @@ btnModalOpen.addEventListener('click', function (evt) {
 })
 btnModalClose.addEventListener('click', function () {
   modalWhole.classList.remove('open');
-  modalFormSection.classList.remove("error");
+  modalFormSection.classList.remove('error');
 });
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (modalWhole.classList.contains("open")) {
+    if (modalWhole.classList.contains('open')) {
       evt.preventDefault();
-      modalWhole.classList.remove("open");
-      modalFormSection.classList.remove("error");
+      modalWhole.classList.remove('open');
+      modalFormSection.classList.remove('error');
     }
   }
 });
 
-modalForm.addEventListener("submit", function (evt) {
+modalForm.addEventListener('submit', function (evt) {
   if (!login.value || !password.value || !message.value) {
     evt.preventDefault();
-    modalFormSection.classList.remove("error");
+    modalFormSection.classList.remove('error');
     modalForm.offsetWidth = modalForm.offsetWidth;
-    modalFormSection.classList.add("error");
+    modalFormSection.classList.add('error');
   }
 });
+
+btnSlider[0].addEventListener('click', function () {
+  clean(wrapper, sliderElement, btnSlider);
+  btnSlider[0].classList.add('active');
+  sliderElement[0].classList.add('active');
+  wrapper.classList.add('wrapper-1');
+})
+
+btnSlider[1].addEventListener('click', function () {
+  clean(wrapper, sliderElement, btnSlider);
+  btnSlider[1].classList.add('active');
+  sliderElement[1].classList.add('active');
+  wrapper.classList.add('wrapper-2');
+})
+btnSlider[2].addEventListener('click', function () {
+  clean(wrapper, sliderElement, btnSlider);
+  btnSlider[2].classList.add('active');
+  sliderElement[2].classList.add('active');
+  wrapper.classList.add('wrapper-3');
+})
